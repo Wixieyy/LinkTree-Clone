5c767fbb820092f47ac9708764417e8c871e8231
Wixieyy <shendrickwilliams@gmail.com>
2024-03-31 15:49:12 +0200

    particle background animation

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("particle-video");
+    if (video) {
+        video.playbackRate = 0.6;
+    }
+});
